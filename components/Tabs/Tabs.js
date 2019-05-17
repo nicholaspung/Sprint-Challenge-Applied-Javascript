55da804e8bd7a1df506e0727e4936cc7976639ad
Nicholas Pung <nicholaspung>
2019-05-17 08:38:14 -0700

    WIP: was able to deselect tab items, however introduced a bug where tabs are clicked twice

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,6 +2,7 @@ class TabLink {
   constructor(tabElement){
     // assign this.tabElement to the tabElement DOM reference
     this.tabElement = tabElement;
+    console.log(this.tabElement);
 
     // Get the `data-tab` value from this.tabElement and store it here
     this.tabData = this.tabElement.dataset.tab; 
@@ -11,11 +12,9 @@ class TabLink {
     if(this.tabData == "all"){
       // If `all` is true, select all cards regardless of their data attribute values
       this.cards = this.tabElement;
-      console.log(this.cards)
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
       this.cards = this.tabElement.dataset;
-      console.log(this.cards)
     }
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. 
@@ -29,22 +28,23 @@ class TabLink {
   selectTab(){
 
     // Select all elements with the .tab class on them
-    // const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll('.tab');
     
     // Iterate through the NodeList removing the .active-tab class from each element
-    // tabs.forEach()
+    tabs.forEach(e => e.classList.remove('active-tab'));
+    console.log(tabs);
 
     // Select all of the elements with the .card class on them
-    // const cards = ;
+    const cards = document.querySelectorAll('.card');
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    // cards.forEach()
+    cards.forEach(e => e.style.display = 'none');
     
     // Add a class of ".active-tab" to this.tabElement
-    // this.tabElement;
+    this.tabElement = this.tabElement.classList.add('active-tab');
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-    // this.cards.forEach(card => card.selectCard());
+    this.cards.forEach(card => card.selectCard());
   }
 }
 
