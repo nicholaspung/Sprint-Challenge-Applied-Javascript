65518a7bc1b85650c8fabe48160f5af9cbb495b1
Nicholas Pung <nicholaspung>
2019-05-17 15:16:14 -0700

    made a rudimentary carousel w/o animation

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,7 +2,40 @@ class Carousel {
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+let carouselImg = carousel.querySelectorAll('img');
+let prevBtn = carousel.querySelector('.left-button');
+let nextBtn = carousel.querySelector('.right-button');
+
+let currentIndex = 0;
+carouselImg[currentIndex].style.display = 'block'
+
+// if statement for end of index
+//console.log(Array.from(carouselImg).includes(carouselImg[3].nextElementSibling)) // false
+
+nextBtn.addEventListener('click', () => {
+    carouselImg[currentIndex].style.display = 'none';
+    // targets next Index
+    currentIndex += 1;
+    carouselImg[currentIndex].style.display = 'block'
+})
+
+prevBtn.addEventListener('click', () => {
+    carouselImg[currentIndex].style.display = 'none';
+    // targets prev Index
+    currentIndex -= 1;
+    carouselImg[currentIndex].style.display = 'block'
+})
+// when page first loads, add display [0]
+// when right button is clicked, remove display [0] and add display [1]
+// when left button is clicked, remove display [1] and add display [0]
+
+
+
+
+
+
+// carouselImg.forEach(e => e.style.display = 'block');
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
